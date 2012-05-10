32de3eb8d7ce4f638697cb11034efcb7f5195a8a
Group 4 <HUANGTC@UCI.EDU>
2012-05-09 18:41:52 -0700

    Committing initial Xcode project.

---
+++ Group4Project/Group4ProjectTests/Group4ProjectTests.h
@@ -0,0 +1,13 @@
+//
+//  Group4ProjectTests.h
+//  Group4ProjectTests
+//
+//  Created by Kojiro Umezaki on 5/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Group4ProjectTests : SenTestCase
+
+@end
---
+++ Group4Project/Group4ProjectTests/Group4ProjectTests.m
@@ -0,0 +1,32 @@
+//
+//  Group4ProjectTests.m
+//  Group4ProjectTests
+//
+//  Created by Kojiro Umezaki on 5/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Group4ProjectTests.h"
+
+@implementation Group4ProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Group4ProjectTests");
+}
+
+@end
