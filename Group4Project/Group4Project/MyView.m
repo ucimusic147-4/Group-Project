c6a2b830154954965ff3fbc2dfe03f764228dec0
huangtc <huangtc@dhcp209b-225.arts.uci.edu>
2012-05-30 16:54:22 -0700

    added first responder and accelerometer functions in myview

--- Group4Project/Group4Project/MyView.m
+++ Group4Project/Group4Project/MyView.m
@@ -124,4 +124,31 @@ extern Singleton* gSing;
 {
 }
 
+/* accelerometer becoming first responder
+
+- (BOOL)canBecomeFirstResponder {
+    return YES;
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [self becomeFirstResponder];
+}
+
+// accelerometer functions
+
+- (void)motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event
+{
+    
+}
+
+- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event
+{
+}
+
+- (void)motionCancelled:(UIEventSubtype)motion withEvent:(UIEvent *)event
+{
+}
+
+*/
+
 @end
